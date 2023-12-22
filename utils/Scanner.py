4bb796810ca472eb7c9242b2100a6334e439fe97
mahdizynali <zeinali.mahdy@gmail.com>
2023-12-22 16:31:27 +0330

    stable single thread

--- utils/Scanner.py
+++ utils/Scanner.py
@@ -2,10 +2,10 @@ import socket
 import time
 import requests
 from termcolor import colored, cprint
-from multiprocessing import Pool
 
-class scanner :
-    '''test aliveness , upload speed, download speed'''
+class scanner:
+    '''test aliveness, upload speed, download speed'''
+
     def __init__(self, rangeIP, port, epoch, num_processes) -> None:
         self.rangeIP = rangeIP
         self.port = port
@@ -13,76 +13,92 @@ class scanner :
         self.num_processes = num_processes
         self.ip_address = []
         self.generate_ip()
-        with Pool(processes=num_processes) as pool:
-            pool.map(self.handler, self.ip_address)
-        
+        self.scan_ips()
+
     def generate_ip(self):
-        for ip in range(self.epoch) :
+        for ip in range(self.epoch):
             self.ip_address.append(self.rangeIP + str(ip))
-            
-    def ip_scanner(self, ip) -> bool :
-        ''' check whether is an ip alive or not '''
-        
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        socket.setdefaulttimeout(1)
-        res = sock.connect_ex((ip, 443))
-        if res == 0:
-            return True
-        
-    def upload_speed (self, ip) -> None :
-        ''' send packet into ip ip address in order to check upload speed time '''
-        
+
+    def ip_scanner(self, ip) -> bool:
+        ''' check whether an IP is alive or not '''
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.settimeout(1)
+            res = sock.connect_ex((ip, 443))
+            return res == 0
+        except socket.error as e:
+            print(f"Error scanning {ip}: {e}")
+            return False
+        finally:
+            sock.close()
+
+    def upload_speed(self, ip) -> None:
+        ''' send packet into ip ip address to check upload speed time '''
         session_up = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        socket.setdefaulttimeout(1)
-        packet = b"a" * 100000 # 0.1 MB of data
-        try :
-            session_up.connect((ip, 443))
-            t0 = time.time()
-            session_up.send(packet)
-            session_up.close()
-            cprint("upload time : " + time.time() - t0, "green")
-            return (time.time() - t0)
-        except :
-            return False 
-
-        
-    def download_speed(self, n_bytes: int,timeout: int, ips) -> None:
-        ''' download initial mount of bytes from specific address to check download speed time '''
-        
-        try :
-            start_time = time.perf_counter()
-            r = requests.get(
-                url=f"http://{ips}/__down",
-                params={"bytes": n_bytes},
-                timeout=timeout,
-                headers={"Host": "speed.cloudflare.com"}
-            )
-            total_time = time.perf_counter() - start_time
-            cf_time = float(r.headers.get("Server-Timing").split("=")[1]) / 1000
-            latency = r.elapsed.total_seconds() - cf_time
-            download_time = total_time - latency
-
-            mb = n_bytes * 8 / (10 ** 6)
-            download_speed = mb / download_time
-
-            cprint("download speed : "+ download_speed, "green")
-            # cprint("latency : "+ latency, "green")
-            return download_speed
-        except :
+        # socket.setdefaulttimeout(1)
+        session_up.settimeout(1)
+        packet = b"a" * 100000  # 0.1 MB of data
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as session_up:
+                session_up.settimeout(1)
+                session_up.connect((ip, 443))
+                packet = b"a" * 100000  # 0.1 MB of data
+                t0 = time.time()
+                session_up.sendall(packet)
+                session_up.recv(1024)  # Add this line to receive acknowledgment from the server
+                upload_time_seconds = time.time() - t0
+                cprint(f"Upload time: {upload_time_seconds:.6f} seconds", "blue")
+                return upload_time_seconds
+        except:
+            cprint(f"Upload time : None", "blue")
             return False
 
-    def handler(self, ip_address) -> None :
+    def download_speed(self, n_bytes: int, timeout: int, ips) -> None:
+        ''' download initial amount of bytes from a specific address to check download speed time '''
+        try:
+            start_time = time.perf_counter()
+            url = f"http://{ips}/__down"
+            headers = {"Host": "speed.cloudflare.com"}
+            params = {"bytes": n_bytes}
+
+            r = requests.get(url, params=params, timeout=timeout, headers=headers)
+
+            if r.status_code == 200:
+                server_timing_header = r.headers.get("Server-Timing")
+                if server_timing_header:
+                    cf_time = float(server_timing_header.split("=")[1]) / 1000
+                    total_time = time.perf_counter() - start_time
+                    latency = r.elapsed.total_seconds() - cf_time
+                    download_time = total_time - latency
+
+                    mb = n_bytes * 8 / (10 ** 6)
+                    download_speed = mb / download_time
+
+                    cprint("Download speed: {:.2f} Mbps".format(download_speed), "blue")
+                    return download_speed
+
+            cprint("Download speed: None", "blue")
+            return False
+
+        except :
+            cprint(f"Download speed: None", "blue")
+            return False
+
+    def handler(self, ip_address) -> None:
         ''' pass elements into processor functions and printout results'''
-        if (self.ip_scanner(ip_address)):
-            up_spead = self.upload_speed(ip_address)
-            down_spead = self.download_speed(1000000, timeout=2, ips=ip_address)
-            if (up_spead != False and down_spead != False) :
-                cprint("alive ip : " + ip_address, "green")
-                print("upload speed : "+ up_spead , "green")
-                print("download speed : "+ down_spead , "green")
+        print("Ip : ", ip_address)
+        if self.ip_scanner(ip_address):
+            up_speed = self.upload_speed(ip_address)
+            down_speed = self.download_speed(1000000, timeout=2, ips=ip_address)
+            if up_speed is not False and down_speed is not False:
+                cprint("status : Alive", "green")
             else:
-                cprint(ip_address + " down !", 'red')
-            print("="*20)
-        else:   
-            cprint(ip_address + " down !", 'red')
-            print("="*20)
+                cprint("Status : down", 'red')
+            print("=" * 40)
+        else:
+            cprint("Status : down", 'red')
+            print("=" * 40)
+
+    def scan_ips(self):
+        for ip_address in self.ip_address:
+            self.handler(ip_address)
